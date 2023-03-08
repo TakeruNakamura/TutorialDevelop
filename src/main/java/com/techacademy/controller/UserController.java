c22e6c23d733b8bf3069e0f7f0542f0be2fd53c2
nakamuratakeru <nakamuratakeru@localhost>
2023-03-09 00:51:53 +0900

    Lesson 17 Chapter7.2 コントローラーの修正

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -3,8 +3,10 @@ package com.techacademy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 
 @Controller
@@ -23,5 +25,15 @@ public class UserController {
         model.addAttribute("userlist", service.getUserList());
         // user/list.htmlに画面遷移
         return "user/list";
+    }
+
+    /** User登録画面を表示 */
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        // User 登録画面に遷移
+        service.saveUser(user);
+
+        // 一覧画面に利害レクト
+        return "redirect:/user/list";
     }
 }

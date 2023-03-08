e9acb772e63c28bc878a382ab452b40129360886
nakamuratakeru <nakamuratakeru@localhost>
2023-03-08 22:45:00 +0900

    Lesson17　Chapter4 途中経過

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user(name, gender, age, email)
+    VALUES ("キラメキ太郎", "男性", 27, 'taro.kirameki@mail.com')
+INSERT INTO user(name, gender, age, email)
+    VALUES ("キラメキ次郎", "男性", 27, 'jiro.kirameki@mail.com')
+INSERT INTO user(name, gender, age, email)
+    VALUES ("キラメキ花子", "女性", 27, 'hanako.kirameki@mail.com')

a8474db9efa304896aba93e1b49e161d95fc74bb
nakamuratakeru <nakamuratakeru@localhost>
2023-03-09 00:40:01 +0900

    Lesson17 Chapter7.1 サービスの修正

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -3,6 +3,7 @@ package com.techacademy.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -15,9 +16,15 @@ public class UserService {
         this.userRepository = repository;
     }
 
-        /** 全件を検索して返す */
-        public List<User> getUserList() {
-            // リポジトリのfindAllメソッドを呼び出す
-            return userRepository.findAll();
-        }
+     /** 全件を検索して返す */
+    public List<User> getUserList() {
+        // リポジトリのfindAllメソッドを呼び出す
+        return userRepository.findAll();
+    }
+
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
